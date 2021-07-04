9fa59342ca66bf0f739d4c1681b910d950cb910b
malay1803 <18acharyamalay@gmail.com>
2021-07-04 23:49:07 +0530

    Random Quote Generator using hooks and quotable API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  const [hex, setHex] = useState("#ffffff");
+
+  useEffect(() => {
+    getQuote();
+    hexColorChange();
+  }, []);
+
+  const getQuote = async () => {
+    const api = "http://api.quotable.io/random";
+    try {
+      const data = await axios.get(api);
+      setQuote(data.data.content);
+      setAuthor(data.data.author);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const hexColorChange = () => {
+    let color = "#";
+    for (let i = 0; i < 3; i++) {
+      color += (
+        "0" +
+        Math.floor(((1 + Math.random()) * Math.pow(16, 2)) / 2).toString(16)
+      ).slice(-2);
+    }
+    setHex(color);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="container" style={{ backgroundColor: `${hex}` }}>
+      <div className="wrapper">
+        <p className="quote">{quote}</p>
+        <p className="quoteMark" style={{ color: `${hex}` }}>
+          ''
         </p>
+        <p className="author">- {author}</p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className="twitterLink"
+          href={`http://twitter.com/intent/tweet?text=${quote}`}
         >
-          Learn React
+          <i className="fab fa-twitter"></i>
+          Tweet
         </a>
-      </header>
+        <button
+          onClick={() => {
+            getQuote();
+            hexColorChange();
+          }}
+          style={{ borderColor: `${hex}`, color: `${hex}` }}
+          className="getQuote"
+        >
+          random quote
+        </button>
+      </div>
     </div>
   );
 }
